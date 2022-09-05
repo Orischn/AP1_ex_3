ffeaa5e1dcccfd65e4f74d605726bc2bec51f295
NadavRam1 <nadavr2005@gmail.com>
2022-09-05 21:47:36 +0300

    made the upload command

--- Commands/uploadCommand.cpp
+++ Commands/uploadCommand.cpp
@@ -1,7 +1,21 @@
 #include "uploadCommand.hpp"
+#include "fileIO.hpp"
+#include "standardIO.hpp"
 
 UploadFileCMD::UploadFileCMD() {
     this->description = "upload an unclassifed csv data file";
 }
 
-void UploadFileCMD::execute() {}
+void UploadFileCMD::execute() {
+    dio->write("Please upload your local train csv file.");
+    FileIO fio(dio->read());
+    std::string data = fio.read();
+    fio.setPath("train.csv");
+    fio.write(data);
+
+    dio->write("Please upload your local test csv file.");
+    fio.setPath(dio->read());
+    data = fio.read();
+    fio.setPath("test.csv");
+    fio.write(data);
+}
