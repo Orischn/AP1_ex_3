761f27cb9839c980fec0da1922fcdc92a64a7df2
Orischn <orischn02@gmail.com>
2022-09-10 19:21:44 +0300

    made the serverside and the cli work with sockets

--- headerFiles/server.hpp
+++ headerFiles/server.hpp
@@ -5,11 +5,10 @@ class Server {
 private:
     int server;
     void bindToPort(int port);
-    void handleDataFromClient(int client);
 public:
     Server(int port);
     ~Server();
-    void acceptClient();
+    int acceptClient();
 };
 
 #endif
--- serverSide/cli.cpp
+++ serverSide/cli.cpp
@@ -2,6 +2,7 @@
 #include "defaultIO.hpp"
 #include "standardIO.hpp"
 #include "fileIO.hpp"
+#include "socketIO.hpp"
 #include "uploadCommand.hpp"
 #include "settingsCommand.hpp"
 #include "classifyCommand.hpp"
@@ -10,27 +11,37 @@
 #include "matrixCommand.hpp"
 #include "settings.hpp"
 
-CLI::CLI() {
+CLI::CLI(int sock) {
+	this->sock = sock;
 	Settings* settings = new Settings();
-	commands.push_back(new UploadFileCMD(new StandardIO()));
-	commands.push_back(new AlgoSettingsCMD(new StandardIO(), settings));
+	commands.push_back(new UploadFileCMD(new SocketIO(sock)));
+	commands.push_back(new AlgoSettingsCMD(new SocketIO(sock), settings));
 	commands.push_back(new ClassifyDataCMD(new FileIO("Commands/output.csv"), settings));
-	commands.push_back(new DisplayResultsCMD(new StandardIO()));
+	commands.push_back(new DisplayResultsCMD(new SocketIO(sock)));
 	commands.push_back(new DownloadResultsCMD());
 	commands.push_back(new AlgorithmConfusionMatrixCMD());
 }
 
 void CLI::start() {
-	StandardIO sio;
+	SocketIO sio(this->sock);
 	while (true) {
+		int input;
 		sio.write("Welcome to the KNN Classifier Server. Please choose an option\n");
 		for (int i = 0; i < commands.size(); i++) {
 			sio.write(std::to_string(i + 1) +". " + (commands[i]->description) + "\n");
 		}
-		int input = std::stoi(sio.read());
+		try {
+			input = std::stoi(sio.read());
+		} catch (const std::exception&) {
+			return;
+		}
 		while (input < 1 || input > 7) {
 			sio.write("Option unavailable! Please try again\n");
-			input = std::stoi(sio.read());
+			try {
+				input = std::stoi(sio.read());
+			} catch (const std::exception&) {
+				return;
+			}
 		}
 		commands[input - 1]->execute();
 	}
